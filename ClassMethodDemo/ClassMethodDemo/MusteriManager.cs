1a3ffe2c1027ef26e1043c769a66c4ce43e9c242
TooWeakTooSloww <78287631+TooWeakTooSloww@users.noreply.github.com>
2021-02-03 04:03:59 +0300

    Add files via upload

---
+++ ClassMethodDemo/ClassMethodDemo/MusteriManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listeye Eklendi");
+        }
+        
+        
+        
+        
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi");
+        }
+        public void Kredi(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + musteri.Soyadi + " isimli müşterinin  kredi puanı = " + musteri.KrediPuani);
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ ClassMethodDemo/ClassMethodDemo/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("************* MÜŞTERİ TAKİBİ UYGULAMASI **********");
+            
+            
+            
+            Musteri mus1 = new Musteri();
+            mus1.Adi = "Bilal";
+            mus1.Soyadi = "Bora";
+            mus1.Id = 003;
+            mus1.KrediPuani = 85;
+
+            Musteri mus2 = new Musteri();
+            mus2.Adi = "Hilal";
+            mus2.Soyadi = "Bora";
+            mus2.Id = 004;
+            mus2.KrediPuani = 95;
+
+            Musteri mus3 = new Musteri();
+            mus3.Adi = "Halil";
+            mus3.Soyadi = "Bora";
+            mus3.Id = 003;
+            mus3.KrediPuani = 60 ;
+
+            Musteri[] musteriler = new Musteri[] { mus1, mus2, mus3 };
+
+            
+
+            Console.WriteLine("MÜŞTERİ BİLGİLERİ");
+
+            foreach (Musteri gezinti in musteriler)
+            {
+                Console.WriteLine("Müşteri Adı = " + gezinti.Adi);
+                Console.WriteLine("Müşteri Soyadı = " + gezinti.Soyadi);
+                Console.WriteLine("Müşteri Id = " + gezinti.Id);
+                Console.WriteLine("Müşteri KrediPuanı = " + gezinti.KrediPuani);
+                Console.WriteLine("---------------------------");            
+            }
+
+
+
+
+
+            Console.WriteLine("\n Müşteri hareketleri");
+
+
+            MusteriManager MusteriManager = new MusteriManager();
+            MusteriManager.Ekle(mus1);
+            MusteriManager.Sil(mus2);
+            MusteriManager.Kredi(mus3);
+            
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
